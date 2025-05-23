9ce1aa96b2040d3e5aa3d9f5da98221ebf183339
Neto <rodriguesarmando225@gmail.com>
2025-05-23 10:15:54 -0300

    Ajustes na tabela 'users'

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,15 @@
 from sqlalchemy import Integer, String, Column, Boolean, DateTime
 from sqlalchemy.orm import relationship
-from app.database import Base
+from app.database import Base   
+from datetime import datetime
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column()
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    senha_hash = Column(String(225), nullable=False)
+    ativo = Column(Boolean, default=True)
+    criado_em = Column(DateTime, default=datetime.utcnow)
+    atualizado_em = Column(DateTime, default=datetime.utcnow)
+    role = Column(String(20), default="vendedor")
